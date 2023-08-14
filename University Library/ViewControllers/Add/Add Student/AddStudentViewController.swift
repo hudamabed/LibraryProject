67c9e93c063872ffffd229cd3bd245ae150ba25b
Huda M. A'abed <hudamabed@gmail.com>
2023-08-14 12:23:33 +0300

    Add New Student file

--- University Library/ViewControllers/Add/Add Student/AddStudentViewController.swift
+++ University Library/ViewControllers/Add/Add Student/AddStudentViewController.swift
@@ -2,47 +2,46 @@
 //  AddStudentViewController.swift
 //  University Library
 //
-//  Created by Huda M. A'abed on 28/07/2023.
+//  Created by Huda M. A'abed on 14/08/2023.
 //
 
 import UIKit
 
-    class AddStudentViewController: UIViewController {
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            setupView()
-            setupData()
-            localized()
-            fetchData()
-        }
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            
-        }
+class AddStudentViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupData()
+        localized()
+        fetchData()
     }
-
-    extension AddStudentViewController {
-        func setupView(){
-        }
-        
-        func localized(){
-            self.title = "Add New Student"
-
-        }
-        
-        func setupData(){
-        }
-        
-        func fetchData(){
-        }
-    }
-
-
-
-    extension AddStudentViewController {
-        
-        
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
     }
+}
 
+extension AddStudentViewController {
+    func setupView(){
+    }
+    
+    func localized(){
+        self.title = "Add New Student"
+        
+    }
+    
+    func setupData(){
+    }
+    
+    func fetchData(){
+    }
+}
+
+
+
+extension AddStudentViewController {
+    
+    
+    
+}
